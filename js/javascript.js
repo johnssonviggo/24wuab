7b60df39bd091290948b88e669cdc03d3d3742d4
johnssonviggo <viggo.johnsson1@gmail.com>
2024-03-20 22:24:14 +0100

    detaljer och javascript

--- js/javascript.js
+++ js/javascript.js
@@ -1,25 +1,8 @@
-const menu = document.querySelector(".menu");
-const menuItems = document.querySelectorAll(".menuItem");
-const hamburger= document.querySelector(".hamburger");
-const closeIcon= document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
+const toggleNavBtn = document.getElementById('toggle-nav');
 
-function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-  } else {
-    menu.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
-  }
+toggleNavBtn.addEventListener('click', toggleNav);
+
+function toggleNav() {
+  let nav = document.querySelector("#header nav");
+  nav.classList.toggle('visible');
 }
-
-hamburger.addEventListener("click", toggleMenu);
-
-menuItems.forEach( 
-  function(menuItem) { 
-    menuItem.addEventListener("click", toggleMenu);
-  }
-)
